9651c815394dc40fbde5ed17d89604c3ac2a7e71
Getaji <t.tanaka.onion@gmail.com>
2019-02-28 22:52:24 +0900

    [Nicknamer][change]Change app location and migration to Vue.js

---
+++ nicknamer/nicknamer.js
@@ -0,0 +1,51 @@
+(function() {
+  const parts = NICKNAMER_PARTS;
+  const parts1 = parts[0];
+  const parts2 = parts[1];
+  const parts1Len = parts1.length;
+  const parts2Len = parts2.length;
+  const patterns = ["12", "2の2", "112", "12の2"];
+  const patternsLen = patterns.length;
+
+  const INITIAL_NAMES_COUNT = 30;
+
+  function randInt(min, max) {
+    return Math.floor( Math.random() * (max - min + 1) ) + min
+  }
+
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        nicknames: this.generateNicknames(INITIAL_NAMES_COUNT),
+        parts1,
+        parts2,
+        namesCount: INITIAL_NAMES_COUNT
+      }
+    },
+    computed: {
+      validateClickGenerate() {
+        return Number.isInteger(this.namesCount) && this.namesCount > 0;
+      }
+    },
+    methods: {
+      generateNickname() {
+        const pattern = patterns[randInt(0, patternsLen - 1)];
+        return pattern.replace(/[12]/g, function(n) {
+          if (n === "1") {
+            return parts1[randInt(0, parts1Len - 1)];
+          } else {
+            return parts2[randInt(0, parts2Len - 1)];
+          }
+        });
+      },
+      generateNicknames(count) {
+        if (count < 1) return [];
+        return new Array(count).fill(0).map(this.generateNickname);
+      },
+      onClickGenerate() {
+        this.nicknames = this.generateNicknames(this.namesCount);
+      }
+    }
+  });
+})();
